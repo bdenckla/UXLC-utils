7103364b19d6aee54f7a24debf061a77979f78f3
Ben Denckla <bdenckla@alum.mit.edu>
2024-02-19 18:24:00 -0500

    tweak intro

--- py/main_amb_early_mtg.py
+++ py/main_amb_early_mtg.py
@@ -16,16 +16,27 @@ def main():
     #
     my_amb_early_mtg_full.write(records)  # fills in path-to-full fields
     #
-    dubious_recs = filter(_has_fem, records)
-    dubious_title = 'Words with dubious early meteg on their 2nd letter'
+    dubious_recs = list(filter(_has_fem, records))
     dubious_path = 'dubious.html'
+    dubious_title = 'Words with dubious early meteg on their 2nd letter'
     my_amb_early_mtg_summary.write(dubious_recs, dubious_path, dubious_title)
     #
-    anch_contents = dubious_title
-    anch_attr = {'href': dubious_path}
-    intro = [my_html.para(my_html.anchor(anch_contents, anch_attr))]
+    intro = _intro(records, dubious_recs, dubious_path, dubious_title)
     title = 'Words with early meteg on their 2nd letter'
     my_amb_early_mtg_summary.write(records, 'index.html', title, intro)
+
+
+def _intro(records, dubious_recs, dubious_path, dubious_title):
+    anch_attr = {'href': dubious_path}
+    nrecs = len(records)
+    ndrecs = len(dubious_recs)
+    para1_contents = f'This page lists all {nrecs} words in UXLC that have an early meteg on their 2nd letter.'
+    anch_contents = f'a version of that list filtered down to the {ndrecs} cases that seem dubious'
+    para2_contents = ['Here is ', my_html.anchor(anch_contents, anch_attr), '.']
+    return [
+        my_html.para(para1_contents),
+        my_html.para(para2_contents),
+    ]
 
 
 
