2f65b5d85124872ff4ac2e096f2a17e1691fea3a
Ben Denckla <bdenckla@alum.mit.edu>
2024-02-22 16:53:08 -0500

    remove some "2nd letter" qualifications
    remove them because they are no longer true,
    now that we include letters beyond the 2nd

--- py/main_amb_early_mtg.py
+++ py/main_amb_early_mtg.py
@@ -26,11 +26,11 @@ def main():
     my_amb_early_mtg_full.write(records)  # fills in path-to-full fields
     #
     dubious_path = 'dubious.html'
-    dubious_title = 'Words with dubious early meteg on their 2nd letter'
+    dubious_title = 'Words with dubious early meteg letter'
     my_amb_early_mtg_summary.write(dubious_recs, dubious_path, dubious_title)
     #
     intro = _intro(records, dubious_recs, dubious_path, dubious_title)
-    title = 'Words with early meteg on their 2nd letter'
+    title = 'Words with ambiguous early meteg'
     my_amb_early_mtg_summary.write(records, 'index.html', title, intro)
 
 
@@ -51,7 +51,7 @@ def _intro(records, dubious_recs, dubious_path, dubious_title):
     anch_attr = {'href': dubious_path}
     nrecs = len(records)
     ndrecs = len(dubious_recs)
-    para1_contents = f'This page lists all {nrecs} words in UXLC that have an early meteg on their 2nd letter.'
+    para1_contents = f'This page lists all {nrecs} words in UXLC that have an ambiguous early meteg.'
     anch_contents = f'a version of that list filtered down to the {ndrecs} cases that seem dubious'
     para2_contents = ['Here is ', my_html.anchor(anch_contents, anch_attr), '.']
     return [
