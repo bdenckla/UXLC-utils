045c6584d25917ee33445b7a17f896deaf4a3d8e
Ben Denckla <bdenckla@alum.mit.edu>
2024-03-06 11:36:56 -0500

    switch back to single quotes

--- py/my_word_diffs_420422_k2q2.py
+++ py/my_word_diffs_420422_k2q2.py
@@ -7,7 +7,7 @@ def _html_for_pcl(pcl):
     return [
         my_html.anchor(
             f'page {page}',
-            {'href': f"https://manuscripts.sefaria.org/leningrad-color/BIB_LENCDX_F{page}.jpg"}
+            {'href': f'https://manuscripts.sefaria.org/leningrad-color/BIB_LENCDX_F{page}.jpg'}
         ),
         f' col {column} line {line}'
     ]
@@ -35,81 +35,81 @@ _EZEK_42_9_PCL = '299B', 3, 22
 EZRA_4_12 = [
     my_html.para(
         [
-            "I think I understand the impulse behind this WLC change, "
-            "but nonetheless I find the change inadvisable. "
-            "So I not only support UXLC’s rejection of this change "
-            "but also advise WLC to revert this change in some future version.",
+            'I think I understand the impulse behind this WLC change, '
+            'but nonetheless I find the change inadvisable. '
+            'So I not only support UXLC’s rejection of this change '
+            'but also advise WLC to revert this change in some future version.',
         ]
     ),
     my_html.para(
         [
-            "My guess is that the impulse behind this change is "
-            "that we are primarily dealing with a word boundary issue here. "
-            "There is a ",
-            rmn("ḥaser/malei"),
-            " issue later in the second word, "
-            "but the primary issue is where the word boundary falls within the letters "
-            "ושוריאשכל[י]לו. "
-            "The table below shows the details."
+            'My guess is that the impulse behind this change is '
+            'that we are primarily dealing with a word boundary issue here. '
+            'There is a ',
+            rmn('ḥaser/malei'),
+            ' issue later in the second word, '
+            'but the primary issue is where the word boundary falls within the letters '
+            'ושוריאשכל[י]לו. '
+            'The table below shows the details.'
         ]
     ),
     my_html.table([
         my_html.table_row([
             my_html.table_header([]),
             my_html.table_header([]),
-            my_html.table_header("word boundary"),
+            my_html.table_header('word boundary'),
         ]),
         my_html.table_row([
-            my_html.table_datum('ketiv'),
-            my_html.table_datum("ושורי אשכללו"),
-            my_html.table_datum("before the א"),
+            my_html.table_datum(rmn('ketiv')),
+            my_html.table_datum('ושורי אשכללו'),
+            my_html.table_datum('before the א'),
         ]),
         my_html.table_row([
-            my_html.table_datum('qere'),
-            my_html.table_datum("ושוריא שכלילו"),
-            my_html.table_datum("after the א"),
+            my_html.table_datum(rmn('qere')),
+            my_html.table_datum('ושוריא שכלילו'),
+            my_html.table_datum('after the א'),
         ])
     ]),
     my_html.para(
         [
-            "Because we are primarily dealing with a word boundary issue, "
-            "it is an understandable impulse to group these 4 words into a single k2q2 construct "
-            "(which is what WLC 4.22 now has) "
-            "rather than group them into a k1q1×2 (two adjacent k1q1 constructs) "
-            "(which is what WLC 4.20 had). "
-            "The table below shows what these constructs look like in Michigan-Claremont terms.",
+            'Because we are primarily dealing with a word boundary issue, '
+            'it is an understandable impulse to group these 4 words into a single k2q2 construct '
+            '(which is what WLC 4.22 now has) '
+            'rather than group them into a k1q1×2 (two adjacent k1q1 constructs) '
+            '(which is what WLC 4.20 had). '
+            'The table below shows what these constructs look like in Michigan-Claremont terms.',
         ]
     ),
     my_html.table([
         my_html.table_row([
             my_html.table_datum('k2q2'),
-            my_html.table_datum("*ka"),
-            my_html.table_datum("*kb"),
-            my_html.table_datum("**qa"),
-            my_html.table_datum("**qb"),
+            my_html.table_datum('*ka'),
+            my_html.table_datum('*kb'),
+            my_html.table_datum('**qa'),
+            my_html.table_datum('**qb'),
         ]),
         my_html.table_row([
             my_html.table_datum('k1q1×2'),
-            my_html.table_datum("*ka"),
-            my_html.table_datum("**qa"),
-            my_html.table_datum("*kb"),
-            my_html.table_datum("**qb"),
+            my_html.table_datum('*ka'),
+            my_html.table_datum('**qa'),
+            my_html.table_datum('*kb'),
+            my_html.table_datum('**qb'),
         ])
     ]),
     my_html.para(
         [
-            "Although the impulse is understandable, it is not consistent with the diplomatic spirit of "
-            "WLC to allow such an impulse to override what we see in the manuscript. "
-            "What we see in the manuscript is the choice to treat these words as k1q1×2: "
-            "two adjacent k1q1 constructs. "
-            "BHS and BHQ agree with the manuscript, though of course we’d be willing to reject their "
-            "reading if it contradicted the manuscript."
+            'Although the impulse is understandable, it is not consistent with the diplomatic spirit of '
+            'WLC to allow such an impulse to override what we see in the manuscript. '
+            'What we see in the manuscript is the choice to treat these words as k1q1×2: '
+            'two adjacent k1q1 constructs. '
+            'BHS and BHQ agree with the manuscript, though of course we’d be willing to reject their '
+            'reading if it contradicted the manuscript.'
         ]
     ),
     my_html.para(
         [
-            "The table below shows two other k2q2 that are instructive to compare, "
-            "since they, too, concern word division."
+            'The table below shows two other k2q2 that are instructive to compare, '
+            'since they, too, concern word division.'
         ]
     ),
     my_html.table([
@@ -124,23 +124,23 @@ EZRA_4_12 = [
     ]),
     my_html.para(
         [
-            "In the case of Ezekiel 42:9, we find that the manuscript supports the k2q2 grouping "
-            "(", *_html_for_pcl(_EZEK_42_9_PCL), "):"
+            'In the case of Ezekiel 42:9, we find that the manuscript supports the k2q2 grouping '
+            '(', *_html_for_pcl(_EZEK_42_9_PCL), '):'
         ]
     ),
     my_html_for_img.html_for_single_img('Ezek42v9.png'),
     my_html.para(
         [
-            "But in the case of 2 Samuel 21:12, we find that the manuscript supports a different k2q2 grouping! "
-            "This k2q2 reaches back one word earlier than the two words of WLC’s k2q2 "
-            "(", *_html_for_pcl(_SND_SAM_21_12_PCL), "):"
+            'But in the case of 2 Samuel 21:12, we find that the manuscript supports a different k2q2 grouping! '
+            'This k2q2 reaches back one word earlier than the two words of WLC’s k2q2 '
+            '(', *_html_for_pcl(_SND_SAM_21_12_PCL), '):'
         ]
     ),
     my_html_for_img.html_for_single_img('2Sam21c12.png'),
     my_html.para(
         [
-            "Using square brackets to set off the k2q2 grouping, here’s how WLC and the manuscript "
-            "divide up the qere letters of these three words:"
+            'Using square brackets to set off the k2q2 grouping, here’s how WLC and the manuscript '
+            'divide up the ', rmn('qere'), ' letters of these three words:'
         ]
     ),
     my_html.table([
@@ -153,4 +153,15 @@ EZRA_4_12 = [
             my_html.table_datum('[תלאום שמה] פלשתים', {'dir': 'rtl'}),
         ]),
     ]),
+    my_html.para(
+        [
+            'Finally, we should admit that ', rmn('qere'), ' grouping '
+            'may have been left up to individual scribal discretion. '
+            'In other words, ', rmn('qere'), ' grouping '
+            'may not have been viewed as meaningful. '
+            'Just as we do not strive to reproduce the occasional elongated ',
+            rmn('lamed'), ' letters that appear in the manuscript, ',
+            'perhaps we should not be concerned to reproduce ', rmn('qere'), ' groupings. '
+        ]
+    ),
 ]
