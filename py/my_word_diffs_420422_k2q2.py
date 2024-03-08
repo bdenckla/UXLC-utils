5535f27ff2718051b0407680b98f50cf98cd0a7a
Ben Denckla <bdenckla@alum.mit.edu>
2024-03-08 16:13:51 -0500

    table and list comparing WLC to א and ל for 2 Sam 5:2

--- py/my_word_diffs_420422_k2q2.py
+++ py/my_word_diffs_420422_k2q2.py
@@ -61,6 +61,7 @@ _SND_SAM_5_2_VMG_ARCHIVE_DOT_ORG_URL = (
 )
 _SND_SAM_5_2_VMG_ARCHIVE_DOT_ORG_ANC = my_html.anchor('full page', {'href': _SND_SAM_5_2_VMG_ARCHIVE_DOT_ORG_URL})
 _HBO_RTL_BIG = {'lang': 'hbo', 'dir': 'rtl', 'class': 'big'}
+_ONE_NONE_3_BIG = [None, _HBO_RTL_BIG, _HBO_RTL_BIG, _HBO_RTL_BIG]
 _GAYA_PAREN_METEG = [rmn('gaʿya'), ' (', rmn('meteg'), ')']
 _JOB_38_1_and_40_6_PARAS = [
     my_html.para([
@@ -366,9 +367,28 @@ EZRA_4_12 = [
         my_html_for_img.html_for_single_img('2Sam5v2-Aleppo.png')
     ]),
     my_html.para([
+        'The table below shows WLC’s pointed qere words and the pointed qere words implied by Mss ל and א.'
+    ]),
+    my_html.table(
+        [
+            my_html.table_row_of_data(['WLC', 'הָיִ֛יתָ', 'הַמּוֹצִ֥יא', 'וְהַמֵּבִ֖יא'], _ONE_NONE_3_BIG),
+            my_html.table_row_of_data(['ל', 'הָיִ֛יתָה', 'הַמּוֹצִ֥יא', 'וְהַמֵּבִ֖יא'], _ONE_NONE_3_BIG),
+            my_html.table_row_of_data(['א', 'הָיִ֛יתָ', 'הַמּוֹצִ֥יא', 'וְהַמֵּבִ֖י'], _ONE_NONE_3_BIG),
+        ],
+        {'dir': 'rtl'}
+    ),
+    my_html.para([
+        'We can summarize this table as follows:'
+    ]),
+    my_html.unordered_list([
+        'WLC agrees with א on word 1, היית.',
+        'All agree on word 2, המוציא.',
+        'WLC agrees with ל word 3, והמביא.'
+    ]),
+    my_html.para([
         'So we could say that WLC’s k1q1×3 in Samuel 5:2 is a mix '
-        'of the traditions we see in ל and א, whereas, '
-        'according to its charter, WLC should hew to ל. '
+        'of the traditions we see in ל and א. '
+        'But according to its charter, WLC should hew to ל. '
         'As to why WLC diverges from ל here, '
         'I very much doubt that א actually influenced BHS. ',
         'Rather, we can see this mix of traditions in '
@@ -379,15 +399,17 @@ EZRA_4_12 = [
     ]),
     my_html.para([
         'This case is typical of most cases where WLC/BHS blatantly diverges from ל. '
-        'In most such cases of blatant divergence from ל, '
+        'In most cases of blatant divergence from ל, '
         'WLC/BHS is conforming to its base text, which, like almost all printed editions '
         'before BHS, hewed close to RB2. '
         'In other words, this is a typical example of a pattern that, as a whole, betrays the methodology of BHS. '
-        'Although one might imagine or hope that BHS was simply a transcription of ל, in fact '
-        'BHS started from an RB2-type text '
-        'and then edited it in most but not all places to conform to ל. '
-        'In a similar way, most modern, general-purpose Hebrew Bibles started from some version of WLC '
-        'and then edited it in most but not all places to conform to general-purpose expectations. '
+        'Although one might imagine or hope that BHS was created by transcribing ל from scratch, in fact '
+        'BHS was created by starting with an RB2-type text '
+        'and editing it in most but not all places to conform to ל. '
+    ]),
+    my_html.para([
+        'In a similar way, most modern, general-purpose Hebrew Bibles were created by starting with some version of WLC '
+        'and then editing it in most but not all places to conform to general-purpose expectations. '
         'This methodoloy is often not publicly disclosed, but certain telltale WLC artifacts leak through, '
         'betraying this methodology.'
     ]),
